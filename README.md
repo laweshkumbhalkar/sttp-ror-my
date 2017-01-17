435dca94851a4891cbfb0365f09b6144d5ef98cf
Your Name <you@example.com>
2017-01-17 17:25:15 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# sttp-ror-my
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+My::Application.config.secret_token = '5c4fa451f78fd866685d77b1a39c3028c0aebd699181b26931816d9ff4ef51757286943e41ae7e198ef4b3314e51d90b64efc89025326ed739a028c3bc0ca862'
